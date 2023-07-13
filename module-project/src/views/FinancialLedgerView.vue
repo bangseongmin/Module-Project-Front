360f455f91368a12de1ba0ebe67cdbbd421779c6
bang-star <bang77060986@gmail.com>
2023-07-13 23:54:06 +0900

    #16 - 페이지 구성

---
+++ module-project/src/views/FinancialLedgerView.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>가계부 페이지</h1>
+</template>
+
+<script>
+export default {
+  name: 'FinancialLeger'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ module-project/src/views/SignUpView.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>회원가입 페이지</h1>
+</template>
+
+<script>
+export default {
+  name: 'SignUpView'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
