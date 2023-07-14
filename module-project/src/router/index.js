1dd9f19746b53cfbb1ee2f8d7749cde9e4988c45
daniel <bangseongmin@naver.com>
2023-07-14 22:31:24 +0900

    #15 - common css and router (#18)

--- module-project/src/router/index.js
+++ module-project/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SignUpView from '../views/SignUpView.vue'
+import FinancialLedgerView from '../views/FinancialLedgerView.vue'
+import TimePlanView from '../views/TimePlanView.vue'
 
 Vue.use(VueRouter)
 
@@ -9,7 +12,27 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
-  }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/SignInView.vue')
+  },
+  {
+    path: '/signUp',
+    name: 'signUp',
+    component: SignUpView
+  },
+  {
+    path: '/plan/wallet',
+    name: 'walletPlan',
+    component: FinancialLedgerView
+  },
+  {
+    path: '/plan/time',
+    name: 'timePlan',
+    component: TimePlanView
+  },
 ]
 
 const router = new VueRouter({
